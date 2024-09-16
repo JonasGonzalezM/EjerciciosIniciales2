88744c745132585b40a35e3a7a22d5da38f9681e
JonasGonzalezM <gonzalezjonas546@gmail.com>
2024-09-16 21:24:10 +0200

    ajustes de hoy

--- Assets/Scripts/Ejercicio2_8.cs
+++ Assets/Scripts/Ejercicio2_8.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class Ejercicio2_8 : MonoBehaviour
 {
     float totalInicial = 120;
+
+    float vidaInicial = 120;
+    float vidaActual;
     // Start is called before the first frame update
     void Start()
     {
-        float turno1 = totalInicial - (120 * 3/100);
-        Debug.Log("Has sido envenenado ahora tienes "+ turno1);
-        float turno2 = totalInicial - (turno1 * 6/100);
-        Debug.Log("Ahora tienes, en el turno 2 " + turno2);
-       float turno3 = totalInicial - (turno2 * 9/100);
-        Debug.Log("Ahora tienes en el turno 3 " + turno3);
-       float turno4 = totalInicial - (turno3 * 12/100);
-        Debug.Log("Ahora tienes en el turno 4 " + turno4);
-        float turno5 = totalInicial- (turno4 * 15/100); 
-        Debug.Log("Ahora tienes en el turno 5 " + turno5);
-
+       // float turno1 = totalInicial - (120 * 3/100);
+       // Debug.Log("Has sido envenenado ahora tienes "+ turno1);
+       // float turno2 = totalInicial - (turno1 * 6/100);
+       // Debug.Log("Ahora tienes, en el turno 2 " + turno2);
+       //float turno3 = totalInicial - (turno2 * 9/100);
+       // Debug.Log("Ahora tienes en el turno 3 " + turno3);
+       //float turno4 = totalInicial - (turno3 * 12/100);
+       // Debug.Log("Ahora tienes en el turno 4 " + turno4);
+       // float turno5 = totalInicial- (turno4 * 15/100); 
+       // Debug.Log("Ahora tienes en el turno 5 " + turno5);
+        float vidaActual = vidaActual - 120*3/100;
         
     }
 
