7e677a73ccf8f732fb6fdbd28e600d19fb66670a
JonasGonzalezM <gonzalezjonas546@gmail.com>
2024-09-27 09:58:06 +0200

    Hasta el ejercicio 7_1

--- Assets/Scripts/Ejercicio6_1.cs
+++ Assets/Scripts/Ejercicio6_1.cs
@@ -8,7 +8,11 @@ public class Ejercicio6_1 : MonoBehaviour
     void Start()
     {
         // a es False||True pero el resultado es True.
-        // b es 
+        // c es True||False pero el resultado es True.
+        // e es True||True y el resultado es True.
+        // b es True && False por lo que el resultado es False
+        // d es True && False por lo que el valor es False
+        // f es True || True y el valor final es True.
         
     }
 
---
+++ Assets/Scripts/Ejercicio6_2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // a es False||False||False por lo que el valor es False
+        // b es False || False por lo que el valor final es False 
+        // c es True && True por lo que el valor es True
+        // d es Flase || True por lo que el valor final es True
+        // e es True || False por lo que el valor final es True
+        // f es False && True por lo que el valor final es False
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
