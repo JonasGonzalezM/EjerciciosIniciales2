a4df26519bbb27b35a5d2ae77c70ecf312e64b80
JonasGonzalezM <gonzalezjonas546@gmail.com>
2024-09-29 18:27:16 +0200

    Update Ejercicio4_3

--- Assets/Scripts/Ejercicio4_3.cs
+++ Assets/Scripts/Ejercicio4_3.cs
@@ -7,20 +7,38 @@ public class Ejercicio4_3 : MonoBehaviour
     [SerializeField] private int dolar;
     [SerializeField] private int euro;
     [SerializeField] private string moneda;
+    [SerializeField] private string monedaDolar;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        string moneda = Convertir( dolar, euro);
+        string moneda = ConvertirEuros(euro);
         Debug.Log(moneda);
+
+
+        string monedaDolar = ConvertirDolares(dolar);
+        Debug.Log(monedaDolar);
     }
 
-     string Convertir(int dolar,int euro)
+     string ConvertirEuros(int euro)
     {
-        string conversion ="Tienes"+ (dolar * 0.631f) + (euro * 0.631f);
-        moneda = conversion;
-        return moneda;
+        //string conversion ="Tienes "+ (dolar * 0.631f) + (euro * 0.631f);
+        //moneda = conversion;
+        //return moneda;
+        float diferencia = euro * 0.1f;
+        string resultadoDolares ="Tienes "+ (euro + diferencia) + " dolares tras pasar de euros a dolares";
+        return resultadoDolares;
+
+
+    }
+
+
+    string ConvertirDolares(int dolar)
+    {
+        float diferencia= dolar * 0.1f;
+        string resultadoEuro="Tienes "+(dolar-diferencia)+ " euros tras pasar de dolar a euro";
+        return resultadoEuro;
     }
     
 }
