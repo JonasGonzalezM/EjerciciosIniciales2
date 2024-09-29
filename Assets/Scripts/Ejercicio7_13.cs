e37dba61832b7ddcade4f653a10dc6b80e145541
JonasGonzalezM <gonzalezjonas546@gmail.com>
2024-09-29 20:55:27 +0200

    Ejercicio 7_13

--- Assets/Scripts/Ejercicio7_13.cs
+++ Assets/Scripts/Ejercicio7_13.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Ejercicio7_13 : MonoBehaviour
 {
+    [SerializeField] private int numero;
+    [SerializeField] private bool primo;
     // Start is called before the first frame update
     void Start()
     {
+        while(numero>=0)
+        {
+            if (numero / 1 == 0)
+            {
+                primo = true;
+                Debug.Log("el numero es primo");
+                
+            }
+            else
+            {
+                primo=false;
+                Debug.Log("el numero no es primo");
+
+            }
+        }
         
     }
 
