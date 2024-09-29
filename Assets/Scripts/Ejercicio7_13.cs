2fe81b372a3dff4818e9396aecb19d8e09235beb
JonasGonzalezM <gonzalezjonas546@gmail.com>
2024-09-29 22:37:17 +0200

    Ejercicio 7_13 realizado

--- Assets/Scripts/Ejercicio7_13.cs
+++ Assets/Scripts/Ejercicio7_13.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 
 public class Ejercicio7_13 : MonoBehaviour
 {
-    [SerializeField] private int numero;
-    [SerializeField] private bool primo;
+    [SerializeField] private int numero=29;
+    //[SerializeField] private int resultado;
+    //[SerializeField] private bool primo=true;
     // Start is called before the first frame update
     void Start()
     {
-        while(numero>=0)
-        {
-            if (numero / 1 == 0)
-            {
-                primo = true;
-                Debug.Log("el numero es primo");
-                
-            }
-            else
-            {
-                primo=false;
-                Debug.Log("el numero no es primo");
 
-            }
-        }
-        
+        int numero = 29; // Cambia este número para probar
+        bool esPrimo = EsPrimo(numero);
+        Debug.Log($"{numero} es primo: {esPrimo}");
+
+        //while(numero>=0&& primo ==true)
+        //{
+        //    resultado = numero / 1;
+        //    if (resultado == 0)
+        //    {
+        //        Debug.Log("el numero es primo");
+        //        primo = true;
+
+        //    }
+
+        //    if(resultado!=0)
+        //    {
+        //        Debug.Log("el numero no es primo");
+        //        primo=false;
+
+        //    }
+        //    break;
+
+        //}
+
     }
 
     // Update is called once per frame
-    void Update()
+    bool EsPrimo(int numero)
     {
-        
+        if (numero < 2) return false; // Los números menores a 2 no son primos
+        for (int i = 2; i * i <= numero; i++)
+        {
+            if (numero % i == 0) return false; // No es primo
+        }
+        return true; // Es primo
     }
 }
