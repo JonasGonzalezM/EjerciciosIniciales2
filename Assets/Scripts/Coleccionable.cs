32de8ff32400b9207e1c30261e4f0d618dde22d9
JonasGonzalezM <gonzalezjonas546@gmail.com>
2024-09-30 20:45:59 +0200

    Rotación completada aunque va un poco lenta

--- Assets/Scripts/Coleccionable.cs
+++ Assets/Scripts/Coleccionable.cs
@@ -7,12 +7,13 @@ public class Coleccionable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.eulerAngles=new Vector3(45, 45,45);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0,3,0)*6*Time.deltaTime);
     }
 }
